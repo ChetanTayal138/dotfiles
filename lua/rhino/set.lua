8d51c60a8f213f5d739b8f7cb4b7a5bba2b8a0e5
ChetanTayal138 <chetantayal138@gmail.com>
2022-09-05 23:23:06 +0530

    updating with undo dir

--- lua/rhino/set.lua
+++ lua/rhino/set.lua
@@ -11,3 +11,12 @@ vim.opt.incsearch = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+    
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true 
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
