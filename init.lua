b3e8918af4b6089c726d6c707ec0464b42ac20a9
ChetanTayal138 <chetantayal138@gmail.com>
2022-09-12 14:12:54 +0530

    adding autocmd for remembering last place

--- init.lua
+++ init.lua
@@ -1,1 +1,24 @@
 require("rhino")
+
+local function nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        vim.api.nvim_command('augroup '..group_name)
+        vim.api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            vim.api.nvim_command(command)
+        end
+        vim.api.nvim_command('augroup END')
+    end
+end
+
+local autocmds = {
+    restore_cursor = {
+        { 'BufRead', '*', [[call setpos(".", getpos("'\""))]] };
+    };
+}
+
+nvim_create_augroups(autocmds)
+-- autocommands END
+
+
