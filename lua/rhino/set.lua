2fc65c1884f01430ef774dfbab8c1144539c3ec7
ChetanTayal138 <chetantayal138@gmail.com>
2022-09-12 14:13:09 +0530

    minors

--- lua/rhino/set.lua
+++ lua/rhino/set.lua
@@ -6,7 +6,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false 
 vim.opt.incsearch = true
 vim.opt.smartindent = true
 
@@ -20,3 +20,4 @@ vim.opt.undofile = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+
